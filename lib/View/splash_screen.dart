a733735a41d3d50c1a3c8063f3c82c1b7f64a771
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2021-12-10 17:48:26 +0500

    initial
    initial

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:  [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
