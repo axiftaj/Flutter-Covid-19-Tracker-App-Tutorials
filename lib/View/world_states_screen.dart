e5d1cbac63e814a9903f1a6a98ba00261e316f3e
Demon4261 <anshmalik279@gmail.com>
2022-07-20 18:15:57 +0530

    app build and splash scrren ready

---
+++ lib/View/world_states_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorldStatesScreen extends StatefulWidget {
+  WorldStatesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WorldStatesScreen> createState() => _WorldStatesScreenState();
+}
+
+class _WorldStatesScreenState extends State<WorldStatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -22,4 +22,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
